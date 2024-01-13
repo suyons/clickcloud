80a66b1383ef3cfe1bc2c606cabcc11f7c724983
suyons <su02ga@outlook.com>
2024-01-13 21:59:16 +0900

    Feat: Controller 추가, 예제 코드 수정
    2024-01-13 21:54
    - Get("/") 요청에 대해 index.html을 반환하는 IndexController 추가
    - Get("/weather") 요청에 대해 날씨 정보를 반환하는 WeatherController 추가
    - /material/frontend 내부 파일에서 Warning을 발생시키는 코드 제거

---
+++ src/main/java/kosmo/clickcloud/WeatherController.java
@@ -0,0 +1,21 @@
+package kosmo.clickcloud;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class WeatherController {
+    @GetMapping("/weather")
+    public String getGlobalWeatherString() {
+        return "";
+    }
+    
+    @PostMapping("/search")
+    public String postCity(@RequestBody String city) {
+        return "";
+    }
+    
+}
