a0f9aaeb34ce360781f46bb5c957864cc5ded4d7
suyons <su02ga@outlook.com>
2024-01-21 16:19:15 +0900

    Feat: JUnit 도입
    2024-01-21 16:19
    1. TestService.java
     - JUnit 테스트 대상 "Hello" 출력 메서드
    2. ServerApplicationTests.java
     - TestService.java를 테스트하는 클래스
    3. junit.md: JUnit 사용법 정리 문서

---
+++ server/src/main/java/clickcloud/server/service/TestService.java
@@ -0,0 +1,10 @@
+package clickcloud.server.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+    public String helloTest() {
+        return "Hello Test";
+    }
+}
--- server/src/test/java/clickcloud/server/ServerApplicationTests.java
+++ server/src/test/java/clickcloud/server/ServerApplicationTests.java
@@ -1,13 +1,21 @@
 package clickcloud.server;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import clickcloud.server.service.TestService;
 
 @SpringBootTest
 class ServerApplicationTests {
+    @Autowired
+    private TestService testService;
 
 	@Test
-	void contextLoads() {
+	public void testHello() {
+        String result = testService.helloTest();
+        assertEquals("Hello Test", result);
 	}
-
 }
