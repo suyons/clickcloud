cf355e145bb06610aec5e672924771bf202bc1e7
suyons <su02ga@outlook.com>
2024-01-22 21:10:36 +0900

    Fix: JDK 버전 21 -> 17 변경, JUnit 삭제
    2024-01-22 21:10
    1. pom.xml: JDK 버전 21 -> 17 변경
    2. TestService.java: JUnit 테스트 대상 클래스 제거
    3. ServerApplicationTests.java: JUnit 테스트 실행 메서드 제거
    4. junit.md: JUnit 사용법 소개 문서 제거

--- server/src/test/java/clickcloud/server/ServerApplicationTests.java
+++ server/src/test/java/clickcloud/server/ServerApplicationTests.java
@@ -1,21 +1,11 @@
 package clickcloud.server;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import clickcloud.server.service.TestService;
 
 @SpringBootTest
 class ServerApplicationTests {
-    @Autowired
-    private TestService testService;
-
-	@Test
-	public void testHello() {
-        String result = testService.helloTest();
-        assertEquals("Hello Test", result);
-	}
+    @Test
+    void contextLoads() {
+    }
 }
